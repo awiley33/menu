643e363f7ca2bf1ff2a22601d46c3003a5371f44
Anna Wiley <awiley33@gmail.com>
2023-07-10 18:56:01 -0500

    feat: Add before blocks to test files and feature testing for index links

--- spec/features/plates/index_spec.rb
+++ spec/features/plates/index_spec.rb
@@ -6,23 +6,17 @@ RSpec.describe "plates index page" do
     chicken_wings = sybergs.plates.create!(name: "Syberg's Famous Wings", price: 10.99, serves: 1, vegetarian: false, category: "entree")
     salad = sybergs.plates.create!(name: "Dinner Salad", price: 4.99, serves: 1, vegetarian: true, category: "side")
 
-      visit "/plates"
+    visit "/plates"
 
-      expect(page).to have_content(chicken_wings.name)
-      expect(page).to have_content(salad.name)
-      expect(page).to_not have_content("taco")
-      
+    expect(page).to have_content(chicken_wings.name)
+    expect(page).to have_content(salad.name)
+    expect(page).to_not have_content("taco")
+  end
 
+  it "has a link for both the restaurant and plate indices at the top of the page" do
+    visit "/plates"
+
+    expect(page).to have_link("Restaurants", :href => "/restaurants")
+    expect(page).to have_link("Plates", :href => "/plates")
   end
 end
-
-
-
-# [ ] done
-
-# User Story 3, Child Index 
-
-# As a visitor
-# When I visit '/child_table_name'
-# Then I see each Child in the system including the Child's attributes
-# (data from each column that is on the child table)
--- spec/features/plates/show_spec.rb
+++ spec/features/plates/show_spec.rb
@@ -1,15 +1,24 @@
 require "rails_helper"
 
 RSpec.describe "plates show page" do
+  before :each do
+    @sybergs = Restaurant.create!(name: "Syberg's", location: "St. Louis, MO", rating: 4.5, reviews: 1765, breakfast: false)
+    @chicken_wings = @sybergs.plates.create!(name: "Syberg's Famous Wings", price: 10.99, serves: 1, vegetarian: false, category: "entree")
+    @salad = @sybergs.plates.create!(name: "Dinner Salad", price: 4.99, serves: 1, vegetarian: true, category: "side")
+  end
+
   it "displays the attributes of a specific plate" do
-    sybergs = Restaurant.create!(name: "Syberg's", location: "St. Louis, MO", rating: 4.5, reviews: 1765, breakfast: false)
-    chicken_wings = sybergs.plates.create!(name: "Syberg's Famous Wings", price: 10.99, serves: 1, vegetarian: false, category: "entree")
-    salad = sybergs.plates.create!(name: "Dinner Salad", price: 4.99, serves: 1, vegetarian: true, category: "side")
+    visit "/plates/#{@chicken_wings.id}"
 
-      visit "/plates/#{chicken_wings.id}"
+    expect(page).to have_content(@chicken_wings.name)
+    expect(page).to_not have_content(@salad.name)
+  end
 
-      expect(page).to have_content(chicken_wings.name)
-      expect(page).to_not have_content(salad.name)
+  it "has a link for both the restaurant and plate indices at the top of the page" do
 
+    visit "/plates/#{@salad.id}"
+
+    expect(page).to have_link("Restaurants", :href => "/restaurants")
+    expect(page).to have_link("Plates", :href => "/plates")
   end
 end
